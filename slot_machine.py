1fc8ea890540dea5116f3efbb460cd67c2540a34
Aiswarya m <121878026+aishwar-ya@users.noreply.github.com>
2026-05-26 19:33:48 +0530

    Added play function and game loop
    Encapsulate slot machine logic in a play function and add a loop for continuous play.

--- slot_machine.py
+++ slot_machine.py
@@ -2,11 +2,21 @@ import random
 
 symbols = ['🍒', '🍇', '🍉', '7️⃣']
 
-results = random.choices(symbols, k=3)
+def play():
+  results = random.choices(symbols, k=3)
 
-print(results[0], "|", results[1], "|", results[2])
+  print(results[0], "|", results[1], "|", results[2])
 
-if results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣':
-  print ("Jackpot!!")
-else:
-  print("Thanks for Playing!!")
+  if results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣':
+      print("Jackpot!!")
+  else:
+      print("Thanks for Playing!!")
+
+while True:
+    play()
+
+    choice = input("Play again? (Y/N): ")
+
+    if choice.upper() == 'N':
+        print("Game Over!")
+        break
